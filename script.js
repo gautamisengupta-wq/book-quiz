662ed9ff3aa48fbdfd5b9758b5e1f173b62e4a2e
gautamisengupta-wq <gautami.sengupta@gmail.com>
2026-04-08 00:20:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
+let score = {
+    fantasy: 0,
+    romance: 0,
+    mystery: 0
+};
+
+let current = 1;
+let totalQuestions = 3;
+
 function answer(type) {
+    score[type]++;
+
+    document.getElementById("q" + current).classList.add("hidden");
+    current++;
+
+    if (current <= totalQuestions) {
+        document.getElementById("q" + current).classList.remove("hidden");
+    } else {
+        showResult();
+    }
+}
+
+function showResult() {
     let result = "";
 
-    if (type === "fantasy") {
-        result = "✨ You should read Harry Potter!";
+    if (score.fantasy > score.romance && score.fantasy > score.mystery) {
+        result = "You should read: Harry Potter";
     } 
-    else if (type === "romance") {
-        result = "💖 You should read Pride and Prejudice!";
+    else if (score.romance > score.mystery) {
+        result = "You should read: Pride and Prejudice";
     } 
     else {
-        result = "🔍 You should read Sherlock Holmes!";
+        result = "You should read: Sherlock Holmes";
     }
 
     document.getElementById("result").innerText = result;
+    document.getElementById("restart").classList.remove("hidden");
 }
